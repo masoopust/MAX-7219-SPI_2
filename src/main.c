a56762f16b6689b6ddd27ed93d2b879e1b37aaa5
Patrik Masopust <mas41320@spseol.cz>
2024-04-22 13:43:50 +0200

    čítání posledního digitu

--- src/main.c
+++ src/main.c
@@ -5,41 +5,91 @@
 #include "milis.h"
 //#include "delay.h"
 //#include "uart1.h"
+#include "max7219.h"
+
+#define DIN_PORT GPIOB
+#define DIN_PIN GPIO_PIN_4
+#define CS_PORT GPIOB
+#define CS_PIN GPIO_PIN_3
+#define CLK_PORT GPIOB
+#define CLK_PIN GPIO_PIN_2
+
 
 void init(void)
 {
     CLK_HSIPrescalerConfig(CLK_PRESCALER_HSIDIV1);      // taktovani MCU na 16MHz
 
-    GPIO_Init(LED_PORT, LED_PIN, GPIO_MODE_OUT_PP_LOW_SLOW);
-#if defined (BTN_PORT) || defined (BTN_PIN)
-    GPIO_Init(BTN_PORT, BTN_PIN, GPIO_MODE_IN_FL_NO_IT);
-#endif
+    GPIO_Init(DIN_PORT, DIN_PIN, GPIO_MODE_OUT_PP_LOW_SLOW);
+    GPIO_Init(CS_PORT, CS_PIN, GPIO_MODE_OUT_PP_HIGH_SLOW);
+    GPIO_Init(CLK_PORT, CLK_PIN, GPIO_MODE_OUT_PP_LOW_SLOW);
 
     init_milis();
     //init_uart1();
 }
 
 
+void display(uint8_t adress, uint8_t data)
+{
+    uint8_t mask ;
+    LOW(CS);            // začátek přenosu
+
+    /* Odesílání adresy */
+    mask = 128;
+    mask = 0b10000000;
+    mask = 1 << 7;               //posun masky 
+    while(mask){
+        if(mask & adress){       //nachystám data
+            HIGH(DIN);
+        }
+        else{
+            LOW(DIN);
+        }
+
+        HIGH(CLK);
+        mask = mask >> 1;
+        LOW(CLK);
+    }
+
+    /* odesílání dat */
+    mask = 0b10000000;
+    mask = 1 << 7;               //posun masky 
+    while(mask){
+        if(mask & data){       //nachystám data
+            HIGH(DIN);
+        }
+        else{
+            LOW(DIN);
+        }
+
+        HIGH(CLK);
+        mask = mask >> 1;
+        LOW(CLK);
+    }
+
+
+    HIGH(CS);  // konec přenosu
+}
+
 int main(void)
 {
   
     uint32_t time = 0;
+    uint8_t number = 0;
 
     init();
 
+    display(DECODE_MODE, 0b11111111);  // zapnutí znakové sady na jednotlivých digitech
+    display(SCAN_LIMIT,7);             // chci všech 8 cifer
+    display(INTENSITY, 1);             // chci intenzitu 1, aby to málo svítilo
+    display(DISPLAY_TEST, DISPLAY_TEST_OFF);
+    display(SHUTDOWN,SHUTDOWN_ON);
+
     while (1) {
-#if defined (BTN_PORT) && defined (BTN_PIN)
-        if (milis() - time > 333 && !PUSH(BTN)) {
-#else
-        if (milis() - time > 333 ) {
-#endif
-            REVERSE(LED); 
+        if (milis() - time > 333) {
             time = milis();
-            //printf("%ld\n", time);
+            display(DIGIT0, number++);
         }
-        //delay_ms(333);
     }
 }
-
 /*-------------------------------  Assert -----------------------------------*/
 #include "__assert__.h"
